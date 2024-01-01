d35d8279eca0449d2216f866c0fae45154c8e903
Ziyaad <xavierbaatjes@yahoo.com>
2024-01-01 11:14:47 +0200

    Update train.py

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def main():
 
     n_gpus = torch.cuda.device_count()
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '8888'
+    os.environ['MASTER_PORT'] = '6066'
 
     hps = utils.get_hparams()
     mp.spawn(run, nprocs=n_gpus, args=(n_gpus, hps,))
